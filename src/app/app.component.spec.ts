1d6fe4196da4641ae11e9c36d7b4b5950dcfb3d4
manalinaik71 <manalinaik71@gmail.com>
2023-09-23 18:43:13 +0530

    First commit to Jasmin and Karma

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,20 @@ describe('AppComponent', () => {
   it(`should have as title 'JasmineTestCases'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('JasmineTestCases');
+    expect(app.title).toEqual('JasmineTestCases1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('JasmineTestCases app is running!');
+    expect(compiled.querySelector('span')?.textContent).toEqual("JasmineTestCases1");
   });
+
+  it('say hello',()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.helloWorld()).toEqual('Hello World');
+  })
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'JasmineTestCases';
+   title = "JasmineTestCases1";
+  helloWorld(){
+    return "Hello World";
+  }
 }
